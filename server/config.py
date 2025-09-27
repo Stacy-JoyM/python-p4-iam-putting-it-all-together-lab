91d2bfd339439134c0f86e7a0d5790e250ddfd88
Stacy <jstacymuthoka@gmail.com>
2025-09-27 15:59:26 +0300

    Fix test

--- server/config.py
+++ server/config.py
@@ -4,24 +4,17 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from flask_session import Session
 
-import warnings
 
-warnings.filterwarnings(
-    "ignore",
-    category=DeprecationWarning,
-    module="flask_session.filesystem.filesystem"
-)
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.secret_key = 'your_super_secret_key' 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-app.config['SECRET_KEY'] = 'supersecretkey'  # required for session
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
+
+
+
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
